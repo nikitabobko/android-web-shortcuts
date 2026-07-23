1e4e6aebefd27984773fb339996f71e1122de002
Nikita Bobko <git@bobko.xyz>
2026-07-23 14:53:23 +0200

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "bobko.webshortcuts"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "2.0.0"
     }
 
     signingConfigs {
